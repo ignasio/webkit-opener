9a3efffac43f06c11788d272b9fb97efd4aa8496
ignasio <ignasio.dpk@gmail.com>
2015-02-07 22:07:00 +0300

    firefox

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,14 +1,11 @@
 class MainController < ApplicationController
   def index
-    position = rand(2)
-    browser_variable_name = $br_arr_keyword[position]
-    browser = $br_arr[position]
-    p browser_variable_name
+    browser = $firefox
     begin
       browser.goto params[:url]
     rescue Selenium::WebDriver::Error::WebDriverError,Errno::ECONNREFUSED
-      browser = Watir::Browser.new browser_variable_name.to_sym  
-      $br_arr[position] = browser
+      browser = Watir::Browser.new :firefox
+      $firefox = browser
       browser.goto params[:url]
     end
     render :text=> browser.html
